acc4a9bf0b4b44208f313cd2577a139ae91e6b95
TylorDidkowsky <48026334+TylorDidkowsky@users.noreply.github.com>
2019-10-29 15:00:49 -0600

    calc1

--- src/01-getting-started/scripts/functions.js
+++ src/01-getting-started/scripts/functions.js
@@ -1,21 +1,25 @@
-
 const functions = {
-    
-    size: (num) => {
-        if (num < 10) return "small";
-        if (num < 20) return "medium";
-        return "large";
-    },
+  size: num => {
+    if (num < 10) return 'small';
+    if (num < 20) return 'medium';
+    return 'large';
+  },
 
-    add: (num1, num2) => {
-        return num1 + num2;
-    },
+  add: (num1, num2) => {
+    console.log(num1 + num2);
+    return num1 + num2;
+  },
 
-    subtract: (num1, num2) => {
-        return num1 - num2;
-    }
+  sub: (num1, num2) => {
+    return num1 - num2;
+  },
 
-    
+  mult: (num1, num2) => {
+    return num1 * num2;
+  },
+  div: (num1, num2) => {
+    return num1 / num2;
+  }
 };
 
 export default functions;
--- src/01-getting-started/scripts/main.js
+++ src/01-getting-started/scripts/main.js
@@ -3,8 +3,34 @@ import functions from './functions.js';
 // **********
 //
 // Add the event listeners
-// 
+//
 
-idNumber.addEventListener('change', (() => {
-    idNumberSize.textContent = functions.size(idNumber.value);
-}));
+idNumber.addEventListener('change', () => {
+  idNumberSize.textContent = functions.size(idNumber.value);
+});
+
+idAdd.addEventListener('click', () => {
+  let userinput1 = parseInt(idNum1.value);
+  let userinput2 = parseInt(idNum2.value);
+  idAnswer.value = functions.add(userinput1, userinput2);
+});
+
+idSub.addEventListener('click', () => {
+  let userinput1 = parseInt(idNum1.value);
+  let userinput2 = parseInt(idNum2.value);
+  idAnswer.value = functions.sub(userinput1, userinput2);
+});
+idMult.addEventListener('click', () => {
+  let userinput1 = parseInt(idNum1.value);
+  let userinput2 = parseInt(idNum2.value);
+  idAnswer.value = functions.mult(userinput1, userinput2);
+});
+idDiv.addEventListener('click', () => {
+  let userinput1 = parseInt(idNum1.value);
+  let userinput2 = parseInt(idNum2.value);
+  idAnswer.value = functions.div(userinput1, userinput2);
+});
+
+idTax.addEventListener('click', () => {
+  let userinput3 = parseInt(idGross.value);
+});
