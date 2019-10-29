d3c2e79944daf9491e1788a78a7a3e485acbfe02
TylorDidkowsky <48026334+TylorDidkowsky@users.noreply.github.com>
2019-10-29 08:44:01 -0600

    calc

---
+++ src/javascript/scripts/10_25_tddmain.js
@@ -0,0 +1,27 @@
+console.log('hello world from main');
+import functions from './10_25_tdd.js';
+functions.helloWorld();
+
+addEventListener('click', () => {
+  let input1 = Number(input1.value);
+  let input2 = Number(input2.value);
+  idanswer.value = functions.sum(input1, input2);
+});
+
+addEventListener('click', () => {
+  let input1 = Number(input1.value);
+  let input2 = Number(input2.value);
+  idanswer.value = functions.sum(input1, input2);
+});
+
+addEventListener('click', () => {
+  let input1 = Number(input1.value);
+  let input2 = Number(input2.value);
+  idanswer.value = functions.sum(input1, input2);
+});
+
+addEventListener('click', () => {
+  let input1 = Number(input1.value);
+  let input2 = Number(input2.value);
+  idanswer.value = functions.sum(input1, input2);
+});
